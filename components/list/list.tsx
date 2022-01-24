c2939dac0f81e5e882abb7696395d43fcb837358
Matt Morgan <m@platypi.io>
2022-01-24 17:27:46 -0500

    retrieving posts

--- components/list/list.tsx
+++ components/list/list.tsx
@@ -1,9 +1,55 @@
 import styles from './list.module.css';
+import { isArray, isEmpty, BASE_URL, getThumbnail } from '../../utils';
 
-export default function List() {
+interface IProps {
+    posts: models.IRedditPost[];
+}
+
+export default function List({ posts }: IProps) {
+    if (!isArray(posts) || posts.length === 0) {
+        return (
+            <div className="pt-10 text-center text-sm">
+                <span>No posts found :(</span>
+            </div>
+        );
+    }
+
     return (
-        <>
+        <div className="max-w-2xl mx-auto pt-1 pb-10">
+            <div>
+                {posts.map((p) => {
+                    const pdata = p?.data;
 
-        </>
+                    return (
+                        <a
+                            className="relative rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm flex items-center space-x-3 hover:border-gray-400 my-2"
+                            href={
+                                isEmpty(pdata.permalink)
+                                    ? BASE_URL
+                                    : BASE_URL + pdata.permalink
+                            }
+                            key={pdata.id}
+                            target="_blank"
+                            rel="noreferrer">
+                            <div className="flex-shrink-0">
+                                <img
+                                    className="h-10"
+                                    src={getThumbnail(pdata.thumbnail)}
+                                    alt=""
+                                />
+                            </div>
+                            <div className="flex-1 min-w-0">
+                                <p className="text-sm font-medium text-gray-900">
+                                    {p.data.title}
+                                </p>
+                                <p className="text-xs text-gray-500 truncate">
+                                    posted by u/{pdata.author}
+                                </p>
+                            </div>
+                        </a>
+                    );
+                })}
+            </div>
+        </div>
     );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,45 +1,110 @@
-import type { NextPage } from 'next';
+import type { NextPage, GetServerSideProps } from 'next';
+import { useState, useEffect } from 'react';
 import Head from 'next/head';
 import Footer from '../components/footer/footer';
 import List from '../components/list/list';
 import Navbar from '../components/navbar/navbar';
 import styles from '../styles/Home.module.css';
+import 'isomorphic-fetch';
+import {
+    ALL_POSTS,
+    BASE_URL,
+    isEmpty,
+    serializeQuery,
+    SUBREDDIT_PATH,
+    SUFFIX,
+} from '../utils';
 
-const Home: NextPage = () => {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>SubReddit</title>
-        <meta name="description" content="A Reddit client that shows posts for subreddits" />
-        <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/favicon/apple-touch-icon.png"
-        />
-        <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicon/favicon-32x32.png"
-        />
-        <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicon/favicon-16x16.png"
-        />
-        <link rel="icon" href="/favicon/favicon.ico" />
-        <link rel="manifest" href="/favicon/site.webmanifest" />
-      </Head>
-      <Navbar />
-      <div className="relative bg-gray-100 pt-24 lg:pt-28 pb-16 min-h-screen">
-        <main>
-          <List />
-        </main>
-        <Footer />
-      </div>
-    </div>
-  )
+interface IProps {
+    postRes: models.IRedditResponse;
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const query = context.query;
+    let subreddit = query.s;
+
+    if (isEmpty(subreddit)) {
+        subreddit = ALL_POSTS;
+    }
+
+    delete query.s;
+
+    try {
+        const response = await fetch(
+            `${BASE_URL}${SUBREDDIT_PATH}${subreddit}${SUFFIX}${serializeQuery(
+                query
+            )}`
+        );
+
+        const jRes: models.IRedditResponse = await response.json();
+
+        return {
+            props: {
+                postRes: jRes,
+            },
+        };
+    } catch (e) {
+        return {
+            props: {
+                postRes: null,
+            },
+        };
+    }
+};
+
+const Home: NextPage<IProps> = ({ postRes }: IProps) => {
+    // const [posts, setPosts] = useState([] as models.IRedditPost[]);
+    // const getPosts = async () => {
+    //   try {
+    //     const res = await fetch(`/api/reddit`);
+    //     const redditResponse: models.IRedditResponse = await res.json();
+    //     setPosts(redditResponse?.data?.children);
+    //   } catch (e) {
+    //     console.log(e);
+    //   }
+    // };
+
+    // useEffect(() => {
+    //   getPosts();
+    // }, []);
+
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>SubReddit</title>
+                <meta
+                    name="description"
+                    content="A Reddit client that shows posts for subreddits"
+                />
+                <link
+                    rel="apple-touch-icon"
+                    sizes="180x180"
+                    href="/favicon/apple-touch-icon.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="32x32"
+                    href="/favicon/favicon-32x32.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="16x16"
+                    href="/favicon/favicon-16x16.png"
+                />
+                <link rel="icon" href="/favicon/favicon.ico" />
+                <link rel="manifest" href="/favicon/site.webmanifest" />
+            </Head>
+            <Navbar />
+            <div className="relative bg-gray-100 pt-24 lg:pt-28 pb-16 min-h-screen">
+                <main>
+                    <List posts={postRes?.data.children} />
+                </main>
+                <Footer />
+            </div>
+        </div>
+    );
 };
 
 export default Home;
